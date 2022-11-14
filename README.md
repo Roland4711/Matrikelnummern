bee5e834fe99550533eef26dc90024c8c36bead5
Roland Reichardt <roland.reichardt@hs-duesseldorf.de>
2022-11-14 12:57:59 +0100

    Zweite Version

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Matrikelnummern
-Toll, was soll ich hier nun schreiben?
+Die Matrikelnummern der Hochschule Düsseldorf (HSD) haben sechs Ziffern.
+Die ersten fünf Ziffern sind signifikant und werden fortlaufend vergeben.
+Die sechste Ziffer ist eine *Prüfziffer*.
+
+In diesem Beispiel werden:
+- Matrikelnummern geprüft (boolean).
+- Prüfziffern von Matrikelnummern berechnet.
+- Matrikelnummern mit korrekten Prüfziffer generieren.
+
+## Algorithmus
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,41 @@
 public class Main {
     public static void main(String[] args) {
-        // Prüfziffer ist richtig
-        int y = berechnePruefziffer(777775);
-        System.out.println(y);
+        int matrNummer = 777777; // Eine sechsstellige Matrikelnummer
+
+        // Prüfen, ob die Matrikelnummer richtig ist
+        boolean istRichtig = checkMatrikelnummer(matrNummer);
+        System.out.println("Prüfung, ob die Matrikelnnumer " + matrNummer + " eine richtige Prüfziffer hat. Ergebnis: " + istRichtig);
+
+        // Korrekte Prüfziffer berechnen
+        int y = berechnePruefziffer(matrNummer);
+        System.out.println("Die richtige Prüfziffer der Matrikelnummer " + matrNummer + " lautet: " + y);
     }
 
+    /**
+     * Prüft, ob von einer Matrikelnummer die letzte Ziffer der berechneten Prüfziffer entspricht
+     * @param mNummer Matrikelnnumer der HSD
+     * @return true, wenn die Prüfziffer korrekt ist.
+     */
+    static boolean checkMatrikelnummer (int mNummer) {
+
+        // Die letzte Ziffer ist die Prüfziffer
+        // Die letzte Ziffer einer Zahl wird bestimmt mit Modulo 10
+        int pZiffer = mNummer % 10;
+
+        // Der Rückgabewert wird bestimmt mit dem Vergleich, ob die
+        // letzte Ziffer der Matrikelnummer mit der Berechnung der
+        // Prüfziffer identisch ist.
+
+        return pZiffer == berechnePruefziffer(mNummer);
+    }
+
+    /**
+     * Berechnet von einer (sechsstelligen) Matrikelnummer die Prüfziffer.
+     * Die Prüfziffer ist richtig, wenn die letzte Ziffer der Matrikelnummer
+     * der berechneten Prüfziffer entspricht
+     * @param m eine Matrikelnummer
+     * @return Prüfziffer der Matrikelnummer
+     */
     static int berechnePruefziffer(int m) {
         int gewSumme = 0;
 
